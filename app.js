6c34f401bc3d0c152254e027e96fe8cf9e1f0134
Edward Kim <itgma1231@gmail.com>
2023-08-15 12:42:14 -0400

    app.js set up

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
 const app = express()
+
 const PORT = 5001
 
+app.use(express.static('./public'))
 
+app.get('/', (req, res) => {
+    res.send('<h1>Home page</h1>')
+})
 
 app.listen(PORT, () => {
     console.log("Server listening on localhost:5001")
