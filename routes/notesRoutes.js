23c09fb4c09d6954eb0f1662b5d86419c66c0709
Edward Kim <itgma1231@gmail.com>
2023-08-16 01:59:23 -0400

    fsPromises.writeFile() new object onto db.json

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,24 +1,33 @@
 const express = require('express');
-const path = require('path')
-const fs = require('fs')
+const path = require('path');
+const fsPromises = require('fs').promises;
 
 // Mock database for notes data storage
-const notesData = require('../db/db.json')
+const notesData = require('../db/db.json');
 
 // Create a router
-const router = express.Router()
+const router = express.Router();
 // Middleware for parsing incoming JSON data on the router
-router.use(express.json())
+router.use(express.json());
 
 // Routes defined for /api/notes
 router.get('/', (req, res) => {
     res.json(notesData)
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     const { title, text } = req.body
     const newNote = { title, text }
-    console.log(newNote)
+    notesData.push(newNote)
+
+    const dbFilePath = path.resolve('db', 'db.json')
+    try {
+        fsPromises.writeFile(dbFilePath, JSON.stringify(notesData, null, 2))
+        console.log(notesData)
+    } catch (error) {
+        console.error("Error writing to mock db.json", error)
+        res.status(500).send("Error saving note")
+    }
 })
 
 router.delete('/:id', (req, res) => {
